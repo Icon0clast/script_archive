77c4e98756ea4bd9fd4c9be63b1aa347a5dbab75
Joe Kulp <joe.kulp@gmail.com>
2018-03-19 12:34:48 -0400

    Add files via upload

---
+++ simpleTcp.py
@@ -0,0 +1,18 @@
+import socket
+
+target_host = "127.0.0.1"
+target_port = 9999
+
+print target_host
+print target_port
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+client.connect((target_host, target_port))
+
+client.send("AABBCC\r\n")
+
+response = client.recv(4096)
+
+print response
